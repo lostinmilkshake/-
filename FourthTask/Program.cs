1a66e9e202effc81a6e16f4e23ec98dd5f566315
Dina The Dinosaur <kirillitvinov@gmail.com>
2019-11-27 12:16:06 +0300

    Started task4

---
+++ FourthTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FourthTask
+{
+    abstract class Ship {
+        private int timeInPort;
+        protected int timeInService;
+        private int amountOfDock;
+        abstract protected void defineTimeOfService();
+    }
+    
+    class SmallShip: Ship {
+        static int minTimeInPort;
+        static int maxTimeInPort;
+
+        protected override void  defineTimeOfService() {
+            this.timeInService = maxTimeInPort - minTimeInPort;
+        }
+    }
+
+    class MiddleShip: Ship {
+        static int minTimeInPort;
+        static int maxTimeInPort;
+        protected override void defineTimeOfService() {
+            this.timeInService = maxTimeInPort - minTimeInPort;
+        }
+    }
+
+    class BigShip: Ship {
+        static int minTimeInPort;
+        static int maxTimeInPort;
+        protected override void defineTimeOfService() {
+            this.timeInService = maxTimeInPort - minTimeInPort;
+        }
+    }
+    
+    class Harbor {
+        private int capacity;
+        private int currentCapacity;
+        private List<Ship> shipQueue;
+        public void addShip() {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SmallShip> smallShipList = new List<SmallShip>();
+            List<MiddleShip> middleShipList = new List<MiddleShip>();
+            List<BigShip> BigShipList = new List<BigShip>();
+            
+        }
+    }
+}
